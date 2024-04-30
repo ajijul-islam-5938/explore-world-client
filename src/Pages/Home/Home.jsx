95ffd21f124946314aab886e4af21a72a807c1f0
shafiul-osd <mdshafiulislamsafi2506@gmail.com>
2024-04-30 08:08:48 +0600

    Country section created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
 import TouriestSpots from '../../Components/TouristSpots/TouristSpots';
+import Country from '../../Components/Contries/Country';
 
 const Home = () => {
     return (
         <div className='my-20'>
             <Banner/>
             <TouriestSpots/>
+            <Country/>
         </div>
     );
 };
